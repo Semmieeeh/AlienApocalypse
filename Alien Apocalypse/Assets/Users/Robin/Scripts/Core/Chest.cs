6020900a6e71e4b2854f0724d7eac5ade16301fc
Robin Kocken <robin.a.kocken@gmail.com>
2023-10-13 16:57:36 +0200

    dskjfbhalsdkjmngas

--- Alien Apocalypse/Assets/Users/Robin/Scripts/Core/Chest.cs
+++ Alien Apocalypse/Assets/Users/Robin/Scripts/Core/Chest.cs
@@ -5,17 +5,59 @@ using Photon.Pun;
 
 public class Chest : MonoBehaviourPunCallbacks, IInteractable
 {
-    public FirearmData[] firearmDatas;
+    public float firearmChance;
+    public float firearmAbilityChance;
+    public float nothingChance;
+
     public Transform goToPoint;
+
+    public GameObject[] firearmDatas;
+    public FirearmAbility[] firearmAbilities;
+    public GameObject abilityHolder;
 
     public void Interact(WeaponInputHandler handler)
     {
-
+        photonView.RPC(nameof(OpenChest), RpcTarget.All);
     }
 
     [PunRPC]
     void OpenChest()
     {
-        //PhotonNetwork.Instantiate(new GameObject, transform.position, new Ve)
+        float value = Random.value;
+
+        if(value >= ((100 - firearmChance) / 100))
+        {
+            int n = Random.Range(0, firearmDatas.Length);
+
+            for(int i = 0; i < firearmDatas.Length; i++)
+            {
+                if(n == i)
+                {
+                    PhotonNetwork.Instantiate(firearmDatas[i].name, goToPoint.position, Quaternion.identity);
+                    return;
+                }
+            }
+        }
+        else if(value >= ((100 - firearmAbilityChance) / 100))
+        {
+            int n = Random.Range(0, firearmAbilities.Length);
+
+            for(int i = 0; i < firearmAbilities.Length; i++)
+            {
+                if(n == i)
+                {
+                    GameObject holder = PhotonNetwork.Instantiate(abilityHolder.name, goToPoint.position, Quaternion.identity);
+
+                    if(holder.TryGetComponent<PickUpAbility>(out PickUpAbility pickUpAbility))
+                    {
+                        pickUpAbility.firearmAbility = firearmAbilities[i];
+                    }
+                }
+            }
+        }
+        else if(value >= ((100 - nothingChance) / 100))
+        {
+            return;
+        }
     }
 }
